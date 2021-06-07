9ea874b41c05c4b24686c775544342a8d4d9b5a6
AndreiChuyanov <achuyanov@gmail.com>
2021-06-07 23:00:16 +0300

    task 4_1

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -2,7 +2,21 @@ import Foundation
 
 public extension Int {
     
-    var roman: String? {
-        nil
+   var roman: String? {
+        guard  self > 0 && self < 4000 else { return nil } 
+	
+		var ret = ""
+        var num = self 
+        let romanNums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        let romanValues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        
+        for i in 0..<13 {
+            while num >= romanNums[i] {
+                num -= romanNums[i] 
+                ret.append(romanValues[i])
+            }
+        }
+        //print (ret)
+        return ret
     }
 }
